8423954f411ec839db8f743e5636e5cf73940bc7
[animangre] <aniketmangre2018@gmail.com>
2023-04-12 17:43:33 +0530

    now4

---
+++ SetterInjectionApp1/src/main/java/com/soft/App.java
@@ -0,0 +1,22 @@
+package com.soft;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.aot.ApplicationContextAotGenerator;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.soft.beans.WelcomeMessageGenrator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/soft/cnfg/applicationContext.xml");
+        System.out.println("start IOC container");
+        WelcomeMessageGenrator genrator = (WelcomeMessageGenrator) context.getBean("messageGenerator");
+        System.out.println("Massage::"+ genrator.messageGenerator("Sanket"));
+    }
+}
