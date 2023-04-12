430990bad2a103adaa68254e2ed18c9760ca5cfa
[animangre] <aniketmangre2018@gmail.com>
2023-04-12 17:42:22 +0530

    now3

---
+++ SetterInjectionApp/src/main/java/com/soft/beans/WelcomeMessageGenrator.java
@@ -0,0 +1,37 @@
+package com.soft.beans;
+
+import java.util.Date;
+
+public class WelcomeMessageGenrator {
+	
+	private Date date;
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String messageGenerator(String name) {
+		
+		
+		int hours = 0;
+		hours = date.getHours();
+		
+		if(hours<=12) {
+			
+			return "Good Morning::"+name;
+			
+		}else if(hours<=16) {
+			
+			return "Good Afternoon::"+name;
+			
+		}else if(hours<=20) {
+			
+			return "Good Evening::"+name;
+		}else {
+			return "Good Night::"+name;
+		}
+		
+	}
+	
+	
+}
